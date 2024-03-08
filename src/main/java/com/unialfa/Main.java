89184f410fc1a1d7e63d99cef73936c8f79fef5b
João Pedro <joaopkrodrigues@gmail.com>
2024-03-07 22:19:44 -0300

    Initial commit

--- src/main/java/com/unialfa/Main.java
+++ src/main/java/com/unialfa/Main.java
@@ -1,7 +1,13 @@
 package com.unialfa;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            var tela = new TelaCadastroPessoa();
+            tela.setVisible(true);
+        });
     }
 }
